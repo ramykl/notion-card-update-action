79be60a13e0c2d9e453bf984c2137c93aac4800c
Ramy <ramy.loveridge@appshack.se>
2022-06-10 21:06:39 +0200

    notion fixes

--- src/notion.ts
+++ src/notion.ts
@@ -2,17 +2,21 @@ import {Client} from '@notionhq/client'
 import * as core from '@actions/core'
 
 const updateCard = async (pageId: string, key: string, value: string) => {
-  core.info(process.env.NOTION_KEY)
+  core.info(process.env.NOTION_KEY || '')
   // Initializing a client
   const notion = new Client({
     auth: process.env.NOTION_KEY
   })
-  const response = await notion.pages.update({
+  let response = await notion.pages.retrieve({
+    page_id: pageId,
+  })
+  core.info(JSON.stringify(response))
+  response = await notion.pages.update({
     page_id: pageId,
     properties: {[key]: value}
   })
-  console.log(`${key} was successfully updated to ${value}`)
-  console.log(response)
+  core.info(`${key} was successfully updated to ${value}`)
+  core.info(response)
 }
 
 export {updateCard}
